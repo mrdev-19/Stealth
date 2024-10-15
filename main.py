3a18cd3ef205f67a0a3e6116509dad0c22bc39a4
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2024-10-15 15:25:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
 # st.code("49QzkutqXiTEgZSzWTVEyXdbZb6EtVJkt8BtHv1UNF3W6mqRhmVPEN4FGe7SxjrTDy4pQ7FKmEub4e9bV544YKWTFXPMX2T",language="python")
+st.write("RVN")
 st.code("RLYVbUBDcAvui8Zwg5KXW6cEUstsoFxr1k",language="python")
+st.write("Code")
+st.code("./xmrig -o asia-rvn.2miners.com:6161 -a rx -k -u RVN:RLYVbUBDcAvui8Zwg5KXW6cEUstsoFxr1k",language="python")
