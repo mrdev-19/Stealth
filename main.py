c60b530b9da47b92f9633dc70551eec562ec7ed5
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2024-10-15 15:56:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# st.code("49QzkutqXiTEgZSzWTVEyXdbZb6EtVJkt8BtHv1UNF3W6mqRhmVPEN4FGe7SxjrTDy4pQ7FKmEub4e9bV544YKWTFXPMX2T",language="python")
+st.code("49QzkutqXiTEgZSzWTVEyXdbZb6EtVJkt8BtHv1UNF3W6mqRhmVPEN4FGe7SxjrTDy4pQ7FKmEub4e9bV544YKWTFXPMX2T",language="python")
 st.write("RVN")
 st.code("RLYVbUBDcAvui8Zwg5KXW6cEUstsoFxr1k",language="python")
 st.write("Code")
